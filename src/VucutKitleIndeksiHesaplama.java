d1e7a1acd79c33cbd7c6b51767ee11f81bdd52da
mervekocc <mervekocaksoy66@gmail.com>
2024-02-21 13:35:47 +0300

    Vucut Kitle İndeks Hesaplama

---
+++ src/VucutKitleIndeksiHesaplama.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class VucutKitleIndeksiHesaplama {
+    public static void main(String[] args){
+        double meter,kg,index;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Lütfen boyunuzu Giriniz :");
+        meter=input.nextDouble();
+        System.out.print("Lütfen Kilonuzu Giriniz :");
+        kg = input.nextDouble();
+        index = kg/(meter*meter);
+        System.out.print("Vücut Kitle İndeksiniz :"+index);
+    }
+}
