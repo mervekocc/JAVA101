2745f80d5f3e64da67d36b9787d29ca93fed3a33
mervekocc <mervekocaksoy66@gmail.com>
2024-02-21 13:51:38 +0300

    Vucut Kitle İndex Hesaplama

--- src/VucutKitleIndeksiHesaplama.java
+++ src/VucutKitleIndeksiHesaplama.java
@@ -5,11 +5,16 @@ public class VucutKitleIndeksiHesaplama {
         double meter,kg,index;
         Scanner input = new Scanner(System.in);
 
+        //Kullanıcıdan bilgileri alıyoruz
         System.out.print("Lütfen Boyunuzu (metre cinsinde) Giriniz :");
         meter=input.nextDouble();
         System.out.print("Lütfen Kilonuzu Giriniz :");
         kg = input.nextDouble();
+
+        //kullanıcının girmiş olduğ bilgiler ile vücut kitle index formülünü uygula
         index = kg/(meter*meter);
+
+        //Çıkan sonucu ekrana yazdırdık
         System.out.print("Vücut Kitle İndeksiniz :"+index);
     }
 }
