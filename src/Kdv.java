18bff371398e8886ac084c65e6d71ce6c27c5dbc
mervekocc <mervekocaksoy66@gmail.com>
2024-02-21 03:32:19 +0300

    KDV tutarı hesaplama

--- src/Kdv.java
+++ src/Kdv.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 public class Kdv {
     public static void main(String[] args) {
         double tutar ;
-        System.out.println("Lütfen tutarı giriniz.");
+        System.out.print("Lütfen tutarı giriniz:");
         Scanner scanner=new Scanner(System.in);
         tutar =scanner.nextDouble();
         System.out.println("Kdvsiz tutar:"+tutar);
-        System.out.println("Kdv tutarı :" + tutar*18/100);
-        System.out.println("Kdvli tutar :"+ (tutar*18/100+tutar));
+        System.out.println("Kdv tutarı :" + tutar*0.18);
+        System.out.println("Kdvli tutar :"+ (tutar*0.18+tutar));
 
         if (tutar<1000){
-            System.out.println("Yüzde 18'li Kdvli tutar :"+ (tutar*18/100+tutar));}
+            System.out.println("Yüzde 18 Kdvli tutar :"+ (tutar*0.18+tutar));}
         else
-            System.out.println("Yüzde 8'li Kdvli tutar :"+ (tutar*8/100+tutar));
+            System.out.println("Yüzde 8 Kdvli tutar :"+ (tutar*0.8+tutar));
 
 
 
