904976004fb7fd7e26c7036b1e4a7fa91a691f36
mervekocc <mervekocaksoy66@gmail.com>
2024-02-22 13:47:11 +0300

    Mükemmel sayı bulan program

--- src/PerfectNumb.java
+++ src/PerfectNumb.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
-public class PerfectNumber {
+public class PerfectNumb {
     public static void main(String[] args) {
+        //kullanıcıdan tamsayı alarak o sayının mükemmel bir sayı olup olmadığını kontrol eder ve sonucu ekrana yazdırır
         int sayi ;
-        int total = 0;
+        int sonuc = 0;
         Scanner input = new Scanner(System.in);
         sayi = input.nextInt();
+        // kullanıcıdan alınan veriler üzerinde işlem yapılır
+        //Bu döngü, sayının tam bölenlerini bulmak için kullanılacak
         for (int i = 1; i < sayi; i++) {
             if (sayi %i==0){
-                total+=i;
+                sonuc+=i;
             }}
-        if (total==sayi){
+        //veriler sonucunda geri bildirim ver
+        if (sonuc==sayi){
             System.out.println(sayi+" Mükemmel Sayıdır");
         }
         else
