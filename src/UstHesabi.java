5c85c6231a674584493db30634d6cc839c310c21
mervekocc <mervekocaksoy66@gmail.com>
2024-02-27 12:38:49 +0300

    Sayıların Üssünü Bulan Program

--- src/UstHesabi.java
+++ src/UstHesabi.java
@@ -6,15 +6,17 @@ public class UstHesabi {
         int us ;
         int toplam=0;
         Scanner scanner=new Scanner(System.in);
-        System.out.print("Üssünü öğrenmek istediğiniz sayıyi giriniz :");
+        // kullanıcıdan veriler alınır
+        System.out.print("Taban değeri giriniz :");
         sayi= scanner.nextInt();
-        System.out.print("Üssü giriniz :");
+        System.out.print("Üs değerini giriniz :");
         us= scanner.nextInt();
         toplam=sayi;
+        // verilere göre işlem yapılır ve çıktı verilir
         for (int i = 1; i <us ; i++) {
             toplam =toplam*sayi;
 
         }
-        System.out.print("Üslü sayının sonucu :" +toplam);
+        System.out.print("Sonuç :" +toplam);
     }
 }
