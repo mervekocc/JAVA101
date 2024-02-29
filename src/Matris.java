9eaea401452221fbf30488b26d4707a1667a7352
mervekocc <mervekocaksoy66@gmail.com>
2024-03-01 00:26:49 +0300

    matris

--- src/Matris.java
+++ src/Matris.java
@@ -1,15 +1,26 @@
 public class Matris {
+
     public static void main(String[] args) {
-        int [][] dizi = {{2,3,4},{5,6,4},{3,2,1}};
-        int [][] temp= new int[3][3] ;
-        for (int i = 0; i < dizi.length; i++) {
-            for (int j = 0; j < dizi[i].length; j++) {
-                temp [i][j]=dizi[j][i];
-                System.out.print((temp[i][j]+" "));
+
+        int[][] matris = {{2, 3, 4}, {5, 6, 4}};
+
+        System.out.println("Matris : ");
+
+        for (int i = 0; i < matris.length; i++) {
+            for (int j = 0; j <= matris.length; j++) {
+                System.out.print(matris[i][j]);
+            }
+            System.out.println();
+
+        }
+        System.out.println("Transpoze: ");
+
+        for (int i = 0; i <= matris.length; i++) {
+            for (int j = 0; j < matris.length; j++) {
+                System.out.print(matris[j][i]);
             }
             System.out.println();
         }
-
 
     }
 }
