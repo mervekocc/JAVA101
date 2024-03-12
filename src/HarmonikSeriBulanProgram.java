b1417a67faac50408506db4269617d6183b5cd6b
mervekocc <mervekocaksoy66@gmail.com>
2024-03-13 02:34:43 +0300

    Harmonik Seri Bulan Program

---
+++ src/HarmonikSeriBulanProgram.java
@@ -0,0 +1,18 @@
+
+
+import java.util.Scanner;
+
+public class HarmonikSeriBulanProgram {
+    public static void main(String[] args) {
+        double n, toplam = 0;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Harmonik serisini bulmak istediğiniz sayıyı (n) giriniz: ");
+        n = input.nextInt();
+
+        for (double x = 1; x <= n; x++) {
+            toplam += 1 / x;
+        }
+        System.out.print("Cevap: " + toplam);
+    }
+}
