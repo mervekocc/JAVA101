96a0dc161239f336307166ed22213bb8278d7119
mervekocc <mervekocaksoy66@gmail.com>
2024-03-19 02:25:48 +0300

    artıkyıl

--- src/HarmonikSeriBulanProgram.java
+++ src/HarmonikSeriBulanProgram.java
@@ -7,7 +7,7 @@ public class HarmonikSeriBulanProgram {
         double n, toplam = 0;
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Harmonik serisini bulmak istediğiniz sayıyı (n) giriniz: ");
+        System.out.print("Harmonik serisini bulmak istediğiniz sayıyı (n) giriniiz: ");
         n = input.nextInt();
 
         for (double x = 1; x <= n; x++) {
