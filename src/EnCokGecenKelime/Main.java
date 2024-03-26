3e09392b28ab796f038c52282823dfa566d1cc24
mervekocc <mervekocaksoy66@gmail.com>
2024-03-27 01:30:45 +0300

    En Çok Geçen Kelimeyi Bulan Program

---
+++ src/EnCokGecenKelime/Main.java
@@ -0,0 +1,38 @@
+package EnCokGecenKelime;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Kullanıcıdan metni al
+        System.out.print("Lütfen bir metin giriniz: ");
+        String text = scanner.nextLine();
+
+        // Metni kelimelere ayır
+        String[] words = text.split("\\s+");
+
+        // Kelimeleri saymak için HashMap oluştur
+        Map<String, Integer> wordCountMap = new HashMap<>();
+
+        // Kelimeleri döngüyle kontrol et
+        for (String word : words) {
+            // Kelimeyi HashMap'te ara, yoksa ekleyip sayacı 1 yap
+            wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
+        }
+
+        // En çok geçen kelimeyi bul
+        String mostFrequentWord = "";
+        int maxCount = 0;
+        for (Map.Entry<String, Integer> entry : wordCountMap.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                mostFrequentWord = entry.getKey();
+                maxCount = entry.getValue();
+            }
+        }
+
+        // Sonucu ekrana yazdır
+        System.out.println("En çok geçen kelime: " + mostFrequentWord + ", Sayısı: " + maxCount);
+    }
+}
